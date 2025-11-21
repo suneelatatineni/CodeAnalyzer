119112ecf4bd45a85667f9dc5377c07f98d7af53
suneelatatineni <149458029+suneelatatineni@users.noreply.github.com>
2025-11-21 07:15:43 +0000

    Add src/Main.java: print odd numbers

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Print odd numbers from 1 to 19
+        for (int i = 1; i <= 19; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
